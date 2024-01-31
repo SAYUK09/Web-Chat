0222db6c72b0e2c86a4aa1ad354c28ea22aef2a8
Sayuri <sayurikamble123@gmail.com>
2024-01-31 15:52:51 +0530

    feat: basic web socket connection setup

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { socket } from "../socket/socket";
 
 export default function Home() {
-  return <div>Home</div>;
+  console.log(socket.connected, "op");
+
+  useEffect(() => {
+    socket.connect();
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  function join() {
+    console.log("ent");
+    if (socket === null) return null;
+
+    socket.emit("join", 1);
+  }
+
+  return (
+    <div>
+      Home
+      <button onClick={join}>Join</button>
+    </div>
+  );
 }
---
+++ src/socket/socket.js
@@ -0,0 +1,9 @@
+import { io } from "socket.io-client";
+
+// const URL =
+//   process.env.NODE_ENV === "production" ? undefined : "http://localhost:5000";
+
+export const socket = io("ws://localhost:5000", {
+  transports: ["websocket"],
+  autoConnect: false,
+});
