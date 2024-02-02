34a0aaf2b41df6d96f5bea404aa217cbc3998f36
Sayuri <sayurikamble123@gmail.com>
2024-02-02 14:50:29 +0530

    fix: deployment errors

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -3,9 +3,10 @@ import { io } from "socket.io-client";
 // const URL =
 //   process.env.NODE_ENV === "production" ? undefined : "http://localhost:5000";
 
-const socketURL = backendUrl.startsWith("http")
-  ? `ws${backendUrl.slice(4)}`
-  : `ws://${backendUrl}`;
+const backendURL = import.meta.env.VITE_BACKEND_URL;
+const socketURL = backendURL.startsWith("http")
+  ? `ws${backendURL.slice(4)}`
+  : `ws://${backendURL}`;
 
 export const socket = io(socketURL, {
   transports: ["websocket"],
